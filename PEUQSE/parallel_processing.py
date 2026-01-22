6cf542698879a8b609ba232c8dae00c24fb0b235
Sevy Harris <sevy.harris@gmail.com>
2026-01-22 16:40:44 -0500

    add dill handling so functions can be passed through MPI

--- PEUQSE/parallel_processing.py
+++ PEUQSE/parallel_processing.py
@@ -1,5 +1,15 @@
 try:
-    from mpi4py import MPI
+    use_dill = True
+    if use_dill:  # Use this if you get the `TypeError: cannot pickle 'module' object` error
+        import dill
+        global MPI
+        
+        from mpi4py import MPI as _MPI
+
+        _MPI.pickle.__init__(dill.dumps, dill.loads, dill.HIGHEST_PROTOCOL)
+        MPI = _MPI
+    else:
+        from mpi4py import MPI
     rank = MPI.COMM_WORLD.Get_rank()
     numProcessors = MPI.COMM_WORLD.Get_size()
     numSimulations = numProcessors - 1 #Normally, we will use rank 0 for controlling.
@@ -9,7 +19,6 @@ try:
         using_mpi = True
     else:
         using_mpi = True
-        
 except ModuleNotFoundError:
     currentProcessorNumber=0
     numProcessors=1
